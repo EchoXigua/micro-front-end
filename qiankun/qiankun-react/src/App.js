caa73fc156e9771cb1abb29fbe6707d52db4fa7f
yal <98389358+EchoXigua@users.noreply.github.com>
2023-01-18 15:41:41 +0800

    qiankun-react

--- qiankun/qiankun-react/src/App.js
+++ qiankun/qiankun-react/src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom'
+import About from './components/About'
+import Home from './components/Home'
 
 function App() {
   return (
@@ -8,8 +10,8 @@ function App() {
       <Link to="/">首页</Link>&nbsp;
       <Link to="/about">about</Link>
       <Routes>
-        <Route path="/" exact render={() => <h1>首页</h1>}></Route>
-        <Route path="/about" exact render={() => <h1>about页面</h1>}></Route>
+        <Route path="/" exact element={<About></About>}></Route>
+        <Route path="/about" element={<Home></Home>}></Route>
       </Routes>
 
     </BrowserRouter>
